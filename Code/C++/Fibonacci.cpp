5965b5d49ca89352b68154cae898788f9f26da58
X7RYQ <x7ryq.is.cool@gmail.com>
2025-10-21 19:39:03 +0300

    Refactor Fibonacci program structure

--- Code/C++/Fibonacci.cpp
+++ Code/C++/Fibonacci.cpp
@@ -1,7 +1,3 @@
-// Srivishva Melvin, sm25017
-// A19, Given natural number n. Find Fibonacci number f(n). (Fibonacci numbers are: f(1) = 1, f(2) = 1, f(n) = f(n-1) + f(n-2), t. i. f(3)=2, f(4)=3, f(5)=5 ...)
-// Program created on : 23/09/2025
-
 #include <iostream>
 using namespace std;
 int main()
@@ -38,4 +34,5 @@ int main()
         cin >> ok; // Input to continue or exit the program
     } while (ok == 1); // Condition to continue the program
     return 0;
+
 }
